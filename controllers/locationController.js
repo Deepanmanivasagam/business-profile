c9b9e97575c7d6c4ec28275d2332ace0f5e2ae85
deepan6381540084 <deepanmanivasagam@gmail.com>
2025-02-03 00:49:59 -0800

    fix the local changes

--- controllers/locationController.js
+++ controllers/locationController.js
@@ -66,4 +66,85 @@ const getAllCities = async (req, res) => {
     }
 };
 
-module.exports = { createCountry, createState, createCity, getAllCountries, getAllStates, getAllCities };
+const updateCountry = async (req,res)=>{
+    try{
+    const {id} = req.params;
+    const {CountryName} = req.body;
+    const updatecountry = await Country.findByIdAndUpdate(id,{CountryName},{new:true});
+    if(!updatecountry){
+        return res.status(400).json({message:'country id not found'})
+    }
+    res.status(200).json({message:'countryName updated',});
+}catch(error){
+     res.status(400).json({message:error.message});
+}
+}
+
+const updateState = async (req,res)=>{
+    try{
+    const {id} = req.params;
+    const {StateName} = req.body;
+    const updatestate = await State.findByIdAndUpdate(id,{StateName},{new:true});
+    if(!updatestate){
+        return res.status(400).json({message:'state id not found'})
+    }
+    res.status(200).json({message:'StateName updated',});
+}catch(error){
+     res.status(400).json({message:error.message});
+}
+}
+
+const updateCity = async (req,res)=>{
+    try{
+    const {id} = req.params;
+    const {CityName} = req.body;
+    const updatecity = await City.findByIdAndUpdate(id,{CityName},{new:true});
+    if(!updatecity){
+        return res.status(400).json({message:'city id not found'})
+    }
+    res.status(200).json({message:'cityName updated',});
+}catch(error){
+     res.status(400).json({message:error.message});
+}
+}
+
+const deleteCountry = async(req,res)=>{
+    try{
+       const {id} = req.params;
+       const deletecountry =  await Country.findByIdAndDelete(id);
+       if(!deletecountry){
+        return res.status(400).json('id not found');
+       }
+       res.status(200).json({messaage:'country deleted successfully'});
+    }catch(error){
+     res.status(400).json({message:error.messaage});
+    }
+}
+
+const deleteState = async(req,res)=>{
+    try{
+       const {id} = req.params;
+       const deletestate =  await State.findByIdAndDelete(id);
+       if(!deletestate){
+        return res.status(400).json('id not found');
+       }
+       res.status(200).json({messaage:'state deleted successfully'});
+    }catch(error){
+     res.status(400).json({message:error.messaage});
+    }
+}
+
+const deleteCity = async(req,res)=>{
+    try{
+       const {id} = req.params;
+       const deletecity =  await City.findByIdAndDelete(id);
+       if(!deletecity){
+        return res.status(400).json('id not found');
+       }
+       res.status(200).json({messaage:'city deleted successfully'});
+    }catch(error){
+     res.status(400).json({message:error.messaage});
+    }
+}
+
+module.exports = { createCountry, createState, createCity, getAllCountries, getAllStates, getAllCities, updateCountry, updateState, updateCity, deleteCountry, deleteState, deleteCity};
